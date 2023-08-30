6087dceb7cf151b1c981bb94dc28e733340da7e9
Achraf EL Khnissi <achraf.elkhnissi@gmail.com>
2023-08-30 12:47:28 +0100

    adding more checks to brew installation

--- installs/brew.sh
+++ installs/brew.sh
@@ -19,14 +19,39 @@ else
   PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 fi
 
+# check if curl is installed
+if ! command -v curl >/dev/null 2>&1; then
+  echo "Installing curl..."
+  $CMD install curl
+fi
+
+# check if git is installed
+if ! command -v git >/dev/null 2>&1; then
+  echo "Installing git..."
+  $CMD install git
+fi
+
 # Check if brew is installed
 if ! command -v brew >/dev/null 2>&1; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # or
+  # git clone https://github.com/Homebrew/brew homebrew
+  # eval "$(homebrew/bin/brew shellenv)"
+  # brew update --force --quiet
+  # chmod -R go-w "$(brew --prefix)/share/zsh"
+
 fi
 
 # Install Homebrew packages
 brew bundle --file="$TARGET/system/Brewfile"
+
+# if ps is not installed
+if ! command -v ps >/dev/null 2>&1; then
+  echo "Installing ps..."
+  $CMD install procps
+fi
 
 # Check if brew is in PATH
 if ! command -v brew >/dev/null 2>&1; then
